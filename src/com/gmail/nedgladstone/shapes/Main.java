8d5e2c90a8ccdeaefcc5afd4156a181255da33ce
nedgladstone <edwardgladstone@comcast.net>
2021-01-24 14:37:20 -0700

    Files for Java course

---
+++ src/com/gmail/nedgladstone/shapes/Main.java
@@ -0,0 +1,9 @@
+package com.gmail.nedgladstone.shapes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(10, 5);
+        System.out.println("Rectangle r has area " + r.getArea());
+    }
+}
---
+++ src/com/gmail/nedgladstone/shapes/Rectangle.java
@@ -0,0 +1,15 @@
+package com.gmail.nedgladstone.shapes;
+
+public class Rectangle {
+    private double height;
+    private double width;
+
+    public Rectangle(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public double getArea() {
+        return height * width;
+    }
+}
