28d826852b1371b7da158ed9f39db4c929f54814
nedgladstone <edwardgladstone@comcast.net>
2021-01-24 14:51:06 -0700

    Emphasize that height and width are floating point

--- src/com/gmail/nedgladstone/shapes/FunWithShapes.java
+++ src/com/gmail/nedgladstone/shapes/FunWithShapes.java
@@ -3,7 +3,7 @@ package com.gmail.nedgladstone.shapes;
 public class FunWithShapes {
 
     public static void main(String[] args) {
-        Rectangle r = new Rectangle(10, 5);
+        Rectangle r = new Rectangle(10.2, 5.8);
         System.out.println("Rectangle r has area " + r.getArea());
     }
 }
